a0c76a5dd1efe52d55439caab956f3000cbcede7
Durai <60003871+duraihub@users.noreply.github.com>
2020-11-15 12:43:52 +0530

    Update Solution.java

--- Data Structures/Linked Lists/Insert a node at the head of a linked list/Solution.java
+++ Data Structures/Linked Lists/Insert a node at the head of a linked list/Solution.java
@@ -1,1 +1,14 @@
+ static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+        System.out.println(llist);
+        
+       SinglyLinkedListNode node = new SinglyLinkedListNode(data);
+    if (llist == null) {
+         return node;
+    }
+   
 
+    node.next = llist;
+    return node;
+        
+
+    }
