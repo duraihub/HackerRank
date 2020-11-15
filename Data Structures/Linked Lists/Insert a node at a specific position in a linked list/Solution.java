8d019b2acb3b3a689a226b57fc03c58e923a8897
Durai <60003871+duraihub@users.noreply.github.com>
2020-11-15 12:45:24 +0530

    Update Solution.java

--- Data Structures/Linked Lists/Insert a node at a specific position in a linked list/Solution.java
+++ Data Structures/Linked Lists/Insert a node at a specific position in a linked list/Solution.java
@@ -1,1 +1,19 @@
 
+  static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+        SinglyLinkedListNode node=new SinglyLinkedListNode(data);
+//node.data=data;
+node.next=null;
+        if(position==0){
+            node.next=head;
+return node;
+        }
+        else{
+            SinglyLinkedListNode temp=head;
+            for(int i=0;i<position-1;i++){
+                temp=temp.next;
+            }
+            node.next=temp.next;
+            temp.next=node;
+            return head;
+        }
+    }
